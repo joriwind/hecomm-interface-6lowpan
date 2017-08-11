0a4b62418d729bca76646f7e561d4525eeac2853
joriwind <j.winderickx@gmail.com>
2017-08-11 17:21:45 +0200

    compiling ipv6 packets (marschal)

--- sixlowpan.go
+++ sixlowpan.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"log"
 
+	"golang.org/x/net/ipv6"
+
 	"github.com/Lobaro/slip"
 	"github.com/jacobsa/go-serial/serial"
 )
@@ -61,6 +63,8 @@ func Open(config Config) (com io.ReadWriteCloser, err error) {
 	return sl, err
 }
 
+const udpHeaderLen = 8
+
 //Read Read until next packet received
 func (com SLIP) Read(buf []byte) (n int, err error) {
 	for {
@@ -72,19 +76,28 @@ func (com SLIP) Read(buf []byte) (n int, err error) {
 		//Case of \r --> receiving debug lines through slip
 		case 0x0D:
 			if com.config.DebugLevel >= DebugAll {
-				fmt.Printf("Debug packet: %v, isPrefix: %v\n", string(packet), isPrefix)
+				fmt.Printf("Debug packet: %v", string(packet))
 			}
 
 		//No special first character --> receiving payload packets
 		default:
 			if com.config.DebugLevel >= DebugPacket {
-				fmt.Printf("SLIP Packet: %v, isPrefix: %v\n", packet, isPrefix)
+				if len(packet) < (ipv6.HeaderLen + udpHeaderLen) {
+					log.Fatalln("Packet to small to fit ipv6 + udp header!!")
+				}
+				fmt.Printf("SLIP Packet payload: %v, isPrefix: %v\n", packet[ipv6.HeaderLen+udpHeaderLen:], isPrefix)
+				header, err := ipv6.ParseHeader(packet[:ipv6.HeaderLen])
+				if err != nil {
+					fmt.Printf("Error in processing ipv6 header\n")
+				} else {
+					fmt.Printf("Header %v\n", header.String())
+				}
 			}
 			if len(buf) < len(packet) {
 				return 0, fmt.Errorf("Buf to small")
 			}
-			copy(buf, packet)
-			return len(packet), nil
+			copy(buf, packet[ipv6.HeaderLen+udpHeaderLen:])
+			return len(packet) - (ipv6.HeaderLen + udpHeaderLen), nil
 		}
 	}
 }
@@ -96,6 +109,40 @@ func (com SLIP) Write(p []byte) (n int, err error) {
 		return 0, err
 	}
 	return len(p), err
+}
+
+//marschal Compile the ipv6 packet
+func marschal(h ipv6.Header, p []byte) (b []byte, err error) {
+	if h.PayloadLen != len(p) {
+		return b, fmt.Errorf("PayloadLen and payload parameter does not match")
+	}
+	b = make([]byte, (ipv6.HeaderLen + h.PayloadLen))
+	//Big-endian or middle endianess
+	b[0] = byte(h.Version)<<4 | byte(h.TrafficClass)>>4 //version to lower, trafficclass only highbits
+	b[1] = byte(h.TrafficClass)<<4 | byte((h.FlowLabel >> 16))
+	b[2] = byte(h.FlowLabel >> 8)
+	b[3] = byte(h.FlowLabel)
+	b[4] = byte(h.PayloadLen >> 8)
+	b[5] = byte(h.PayloadLen)
+	b[6] = byte(h.NextHeader)
+	b[7] = byte(h.HopLimit)
+	copy(b[8:24], h.Src)
+	copy(b[24:40], h.Dst)
+
+	copy(b[40:40+h.PayloadLen], p)
+	return b, nil
+	/* h := &Header{
+		Version:      int(b[0]) >> 4,
+		TrafficClass: int(b[0]&0x0f)<<4 | int(b[1])>>4,
+		FlowLabel:    int(b[1]&0x0f)<<16 | int(b[2])<<8 | int(b[3]),
+		PayloadLen:   int(binary.BigEndian.Uint16(b[4:6])),
+		NextHeader:   int(b[6]),
+		HopLimit:     int(b[7]),
+	}
+	h.Src = make(net.IP, net.IPv6len)
+	copy(h.Src, b[8:24])
+	h.Dst = make(net.IP, net.IPv6len)
+	copy(h.Dst, b[24:40]) */
 }
 
 //Close Close the connection
