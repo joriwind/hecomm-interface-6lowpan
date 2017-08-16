be35195c1a0d9f33d5199cc9a20cc725e1bfa662
joriwind <j.winderickx@gmail.com>
2017-08-16 14:34:59 +0200

    UDP header + checksum and marschal

---
+++ headers.go
@@ -0,0 +1,137 @@
+package sixlowpan
+
+import (
+	"bytes"
+	"encoding/binary"
+	"net"
+
+	"golang.org/x/net/ipv6"
+)
+
+//UDPHeader Defines the header of UDP packet
+type UDPHeader struct {
+	SrcPort uint16
+	DstPort uint16
+	Length  uint16
+	Chksum  uint16
+	Payload []byte
+}
+
+type udppseudoHeader struct {
+	SrcAddress net.IP
+	DstAddress net.IP
+	UDPLength  uint32
+	Zeroes     uint8
+	NxtHdr     uint8
+}
+
+//Marschal Compile the UDPHeader into byte slice
+func (h UDPHeader) Marschal() ([]byte, error) {
+	//We know the length of the entire UDPheader + payload, h.Lenght should be the comprise lenght of hdr & payload
+	//Size the internal write buffer to h.Length via buf & setting the capacity (=h.Length) but not the length (=0)
+	buf := make([]byte, 0, h.Length)
+	b := bytes.NewBuffer(buf)
+
+	//Checking extensively just because maybe reason
+	var err error
+	err = binary.Write(b, binary.BigEndian, h.SrcPort)
+	if err != nil {
+		return nil, err
+	}
+	err = binary.Write(b, binary.BigEndian, h.DstPort)
+	if err != nil {
+		return nil, err
+	}
+	err = binary.Write(b, binary.BigEndian, h.Length)
+	if err != nil {
+		return nil, err
+	}
+	err = binary.Write(b, binary.BigEndian, h.Chksum)
+	if err != nil {
+		return nil, err
+	}
+	err = binary.Write(b, binary.BigEndian, h.Payload)
+	if err != nil {
+		return nil, err
+	}
+
+	return b.Bytes(), nil
+}
+
+//CalcChecksum Calculate the checksum of UDP header, providing the ip header information for pseudoheader
+func (h *UDPHeader) CalcChecksum(ip *ipv6.Header) error {
+	h.Chksum = 0
+	phdr := udppseudoHeader{
+		SrcAddress: ip.Src.To16(),
+		DstAddress: ip.Dst.To16(),
+		UDPLength:  uint32(h.Length),
+		Zeroes:     0,
+		NxtHdr:     17,
+	}
+	var b bytes.Buffer
+	var err error
+	//Writing first part of pseudo header
+	err = binary.Write(&b, binary.BigEndian, phdr.SrcAddress)
+	if err != nil {
+		return err
+	}
+	err = binary.Write(&b, binary.BigEndian, phdr.DstAddress)
+	if err != nil {
+		return err
+	}
+	err = binary.Write(&b, binary.BigEndian, phdr.UDPLength)
+	if err != nil {
+		return err
+	}
+	for i := 0; i < 3; i++ { //Total of 24bits of data
+		err = binary.Write(&b, binary.BigEndian, phdr.Zeroes)
+		if err != nil {
+			return err
+		}
+	}
+	err = binary.Write(&b, binary.BigEndian, phdr.NxtHdr)
+	if err != nil {
+		return err
+	}
+
+	//Writing second part, also marschal of UDP header
+	bytes, err := h.Marschal()
+	if err != nil {
+		return err
+	}
+	err = binary.Write(&b, binary.BigEndian, bytes)
+	if err != nil {
+		return err
+	}
+
+	//Overwrite checksum in provided UDP header
+	h.Chksum = checksum(b.Bytes())
+	return nil
+}
+
+//Checksum Implementation from: https://gist.github.com/chrisnc/0ff3d1c20cb6687454b0 -- rawudp.go
+func checksum(buf []byte) uint16 {
+	sum := uint32(0)
+
+	for ; len(buf) >= 2; buf = buf[2:] {
+		sum += uint32(buf[0])<<8 | uint32(buf[1])
+	}
+	if len(buf) > 0 {
+		sum += uint32(buf[0]) << 8
+	}
+	for sum > 0xffff {
+		sum = (sum >> 16) + (sum & 0xffff)
+	}
+	csum := ^uint16(sum)
+	/*
+	 * From RFC 768:
+	 * If the computed checksum is zero, it is transmitted as all ones (the
+	 * equivalent in one's complement arithmetic). An all zero transmitted
+	 * checksum value means that the transmitter generated no checksum (for
+	 * debugging or for higher level protocols that don't care).
+	 */
+	if csum == 0 {
+		csum = 0xffff
+	}
+	return csum
+}
--- main_test.go
+++ main_test.go
@@ -2,7 +2,10 @@ package sixlowpan
 
 import (
 	"fmt"
+	"net"
 	"testing"
+
+	"golang.org/x/net/ipv6"
 )
 
 /* func TestRcvPacket(t *testing.T) {
@@ -79,5 +82,90 @@ func TestContinuous(t *testing.T) {
 			}
 			fmt.Printf("Packet: %v\n", string(buf[:n]))
 		}
+	}
+}
+
+func TestIPPacket(t *testing.T) {
+	h := &ipv6.Header{
+		Version:      6,
+		TrafficClass: 0,
+		FlowLabel:    0,
+		PayloadLen:   0,
+		NextHeader:   17,
+		HopLimit:     255,
+	}
+
+	cases := []struct {
+		header  *ipv6.Header
+		dst     net.IP
+		src     net.IP
+		payload []byte
+	}{
+		{header: h, dst: net.ParseIP("aaaa::c30c:0:0:7"), src: net.ParseIP("aaaa::1"), payload: []byte{0x0, 0x0, 0x16, 0x33, 0x0, 0x0A, 0x0, 0x0, 0x48, 0x49}},
+	}
+	for _, c := range cases {
+		c.header.Dst = c.dst
+		c.header.Src = c.src
+		c.header.PayloadLen = len(c.payload)
+		b, err := Marschal(*c.header, c.payload)
+		if err != nil {
+			t.Errorf("Something went wrong in marshalling IP packet: case %v, result: %x, error: %v\n", c, b, err)
+		}
+		fmt.Printf("Case: %v, result: %x\n", c, b)
+
+	}
+}
+
+func TestUDPPacket(t *testing.T) {
+	h := &ipv6.Header{
+		Version:      6,
+		TrafficClass: 0,
+		FlowLabel:    0,
+		PayloadLen:   0x001e,
+		NextHeader:   17,
+		HopLimit:     63,
+		Dst:          net.ParseIP("aaaa::c30c:0:0:7"),
+		Src:          net.ParseIP("aaaa::1"),
+	}
+	uHdr := &UDPHeader{
+		DstPort: 0xb25f,
+		SrcPort: 0x1633,
+		Length:  0x001e,
+		Chksum:  0,
+		Payload: []byte{0x60, 0x45, 0x22, 0xa6, 0x41, 0x0c, 0x80, 0xb1, 0x02, 0xff, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21},
+	}
+
+	cases := []struct {
+		header *ipv6.Header
+		udpHdr *UDPHeader
+		result []byte
+	}{
+		{
+			header: h,
+			udpHdr: uHdr,
+			result: []byte{0x16, 0x33, 0xb2, 0x5f, 0x00, 0x1e, 0x85, 0x1e, 0x60, 0x45, 0x22, 0xa6,
+				0x41, 0x0c, 0x80, 0xb1, 0x02, 0xff, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21}},
+	}
+	for _, c := range cases {
+		err := c.udpHdr.CalcChecksum(c.header)
+		if err != nil {
+			t.Errorf("Something went wrong in calculating checksum UDP packet: case %v, error: %v\n", c, err)
+		}
+		b, err := c.udpHdr.Marschal()
+		if err != nil {
+			t.Errorf("Something went wrong in marshalling UDP packet: case %v, result: %x, error: %v\n", c, b, err)
+		}
+		if c.result != nil {
+			if len(c.result) != len(b) {
+				t.Errorf("Did not correspond to expected result, size error!\n")
+			}
+			for i, x := range b {
+				if c.result[i] != x {
+					t.Errorf("Did not correspond to expected result, content error!\n")
+				}
+			}
+		}
+		fmt.Printf("Succesful:: Case: %v, result: %x\n", c, b)
+
 	}
 }
